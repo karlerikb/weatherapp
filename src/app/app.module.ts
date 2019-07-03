27ceee4d7ff08ce13e5ed3e8c54dbde609c1c0f9
karlerikb <karlerikb@users.noreply.github.com>
2019-07-03 12:05:25 +0300

    added root styling + initialized main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CitySelectionComponent } from './city-selection/city-selection.component';
+import { WeatherInfoComponent } from './weather-info/weather-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CitySelectionComponent,
+    WeatherInfoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/city-selection/city-selection.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-city-selection',
+  templateUrl: './city-selection.component.html',
+  styleUrls: ['./city-selection.component.scss']
+})
+export class CitySelectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
