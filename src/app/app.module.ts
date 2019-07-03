79237032bcb9c430d8a11ebc82b8b1ff8fa34b95
karlerikb <karlerikb@users.noreply.github.com>
2019-07-03 21:00:32 +0300

    added weather info component styling + sass variables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CitySelectionComponent } from './city-selection/city-selection.component';
 import { WeatherInfoComponent } from './weather-info/weather-info.component';
+import { CurrentWeatherComponent } from './weather-info/current-weather/current-weather.component';
+import { TemperatureListComponent } from './weather-info/temperature-list/temperature-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CitySelectionComponent,
-    WeatherInfoComponent
+    WeatherInfoComponent,
+    CurrentWeatherComponent,
+    TemperatureListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/weather-info/temperature-list/temperature-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-temperature-list',
+  templateUrl: './temperature-list.component.html',
+  styleUrls: ['./temperature-list.component.scss']
+})
+export class TemperatureListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
